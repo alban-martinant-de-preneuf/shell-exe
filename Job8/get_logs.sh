57e4c8629b5b09cb25bedcfe04b63d32328200d2
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-09-30 15:57:57 +0200

    affichage de la date dans le fichier décompréssé

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-file="number_connection-"
-date="$(date +%d-%m-%Y-%H:%M)"
+file=("number_connection-$(date +%d-%m-%Y-%H:%M)")
 
 last | grep alban | wc -l > $file
 
-mkdir -p /home/alban/bin/Backup
+mkdir -p /home/alban/Documents/Development/shell-exe/Job8/Backup
 
-tar -cf /home/alban/bin/Backup/$file.tar $file
-mv /home/alban/bin/Backup/$file.tar /home/alban/bin/Backup/$file$date.tar
+tar -cf /home/alban/Documents/Development/shell-exe/Job8/Backup/$file.tar $file
 
-rm /home/alban/bin/$file
+rm $file 
